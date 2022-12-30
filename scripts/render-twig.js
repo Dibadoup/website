7ed8dac99db953edc7d56819191521ce53241970
William Desportes <williamdes@wdes.fr>
2022-12-30 11:15:51 +0400

    Improve render script

--- scripts/render-twig.js
+++ scripts/render-twig.js
@@ -8,16 +8,15 @@ const prettier = require('prettier');
 module.exports = function rendertwig(filePath) {
     const destPath = filePath.replace(/src\/twig\//, 'dist/').replace(/\.twig$/, '.html');
     const srcPath = upath.resolve(upath.dirname(__filename), '../src');
+    const destPathDirname = upath.dirname(destPath);
+    if (!sh.test('-e', destPathDirname)) {
+        sh.mkdir('-p', destPathDirname);
+    }
 
     console.log(`### INFO: Rendering ${filePath} to ${destPath}`);
-    const html = twig.renderFile(filePath, {
+    twig.renderFile(filePath, {
         // vars
     }, (err, html) => {
-        const destPathDirname = upath.dirname(destPath);
-        if (!sh.test('-e', destPathDirname)) {
-            sh.mkdir('-p', destPathDirname);
-        }
-
         const prettified = prettier.format(html, {
             printWidth: 1000,
             tabWidth: 4,
@@ -29,5 +28,6 @@ module.exports = function rendertwig(filePath) {
         });
 
         fs.writeFileSync(destPath, prettified);
+        console.log(`### INFO: Rendered ${filePath} to ${destPath}`);
     });
 };
